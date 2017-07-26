ea208bfa97835525af90513fdb8642538917cf9a
esteban1234 <tva_libra@hotmail.com>
2017-07-26 13:31:33 -0500

    metas

--- vistas/encabezado.php
+++ vistas/encabezado.php
@@ -2,7 +2,7 @@
 <html lang="es">
 <head>
 	<meta charset="UTF-8">
-	
+	<meta name="google-site-verification" content="8upD0ZL7USmYbnJFZrPQqxsjQa8dlURxn7690meSikM" />
 	<link rel="icon" type="image/png" href="img/favicon.png" />
 	<title>GRUPO DE SERVICIOS HOSPITALARIOS BEDA</title>
 	 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"> 
