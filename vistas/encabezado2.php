032589a73be6b20713ab68f9bf704ef672e2f209
champiiy <albert1047@hotmail.com>
2017-07-31 18:35:54 -0500

    UPDATE ERROR

--- vistas/encabezado2.php
+++ vistas/encabezado2.php
@@ -2,10 +2,10 @@
 <html lang="es">
 <head>
 	<meta charset="UTF-8">
-	
+
 	<link rel="icon" type="image/png" href="../img/favicon.png" />
 	<title>GRUPO DE SERVICIOS HOSPITALARIOS BEDA</title>
-	 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"> 
+	 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
   	<META NAME="Title" content="GRUPO DE SERVICIOS HOSPITALARIOS BEDA">
   	<META NAME="Author" CONTENT="[http://beda.com.mx/]"/>
   	<META name="Description" content="Empresa Social del Estado que presta  servicios de salud de alta calidad y seguridad."/>
